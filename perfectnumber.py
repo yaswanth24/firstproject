7ad20f82c998e581c9229b0d23e4958752a2d2f2
yaswanth <ajji.yaswanth.kumar@gmail.com>
2019-02-05 22:06:00 +0530

    perfectnumber commit

---
+++ perfectnumber.py
@@ -0,0 +1,11 @@
+def perfectFunc():
+    n=int(input("enter a number"))
+    m=0
+    for i in range(1,n):
+        if n%i==0:
+            m=m+i
+    if m==n:
+        print("{} is perfect number".format(n))
+    else:
+        print("{} is not a perfect number".format(n))
+perfectFunc()
